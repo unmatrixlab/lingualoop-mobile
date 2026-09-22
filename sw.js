0c55977de4d6de9f67358dd0de63803a7bb15c64
unmatrixlab <unmatrixlab@gmail.com>
2026-09-22 15:44:20 -0400

    Refresh the offline mobile cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'lingualoop-mobile-v2';
-const ASSETS = ['./', './index.html', './styles.css?v=1', './app.js?v=1', './manifest.webmanifest', './icon.svg'];
+const CACHE = 'lingualoop-mobile-v3';
+const ASSETS = ['./', './index.html', './styles.css?v=2', './app.js?v=2', './manifest.webmanifest', './icon.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
