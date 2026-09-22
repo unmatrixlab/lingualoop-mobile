2d758a7a5fcd5b0f776e223d58559b62c9e3977b
unmatrixlab <unmatrixlab@gmail.com>
2026-09-22 17:48:22 -0400

    Refresh mobile video controls

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'lingualoop-mobile-v9';
-const ASSETS = ['./', './index.html', './styles.css?v=6', './app.js?v=7', './manifest.webmanifest', './icon.svg'];
+const CACHE = 'lingualoop-mobile-v10';
+const ASSETS = ['./', './index.html', './styles.css?v=7', './app.js?v=7', './manifest.webmanifest', './icon.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
