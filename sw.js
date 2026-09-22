2aa84192c749c5fec176ffc1f6e0e35858106d71
unmatrixlab <unmatrixlab@gmail.com>
2026-09-22 16:17:53 -0400

    Refresh offline assets for iPhone transfer

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'lingualoop-mobile-v3';
-const ASSETS = ['./', './index.html', './styles.css?v=2', './app.js?v=2', './manifest.webmanifest', './icon.svg'];
+const CACHE = 'lingualoop-mobile-v5';
+const ASSETS = ['./', './index.html', './styles.css?v=3', './app.js?v=4', './manifest.webmanifest', './icon.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
