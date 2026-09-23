e16bedb6684085722c8807a4a52299154f111de0
unmatrixlab <unmatrixlab@gmail.com>
2026-09-23 00:07:51 -0400

    Update cache version and app.js version in sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'lingualoop-mobile-v11';
-const ASSETS = ['./', './index.html', './styles.css?v=8', './app.js?v=8', './manifest.webmanifest', './icon.svg'];
+const CACHE = 'lingualoop-mobile-v12';
+const ASSETS = ['./', './index.html', './styles.css?v=8', './app.js?v=9', './manifest.webmanifest', './icon.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
