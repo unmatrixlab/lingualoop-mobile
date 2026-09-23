f79a63122f1cbeb26dc9848333665857c7f12c8b
unmatrixlab <unmatrixlab@gmail.com>
2026-09-23 01:13:21 -0400

    Refresh mobile app cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'lingualoop-mobile-v13';
-const ASSETS = ['./', './index.html', './styles.css?v=8', './app.js?v=10', './manifest.webmanifest', './icon.svg'];
+const CACHE = 'lingualoop-mobile-v14';
+const ASSETS = ['./', './index.html', './styles.css?v=8', './app.js?v=11', './manifest.webmanifest', './icon.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
